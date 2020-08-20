74984027270cb3e56838cfa201f709228a4d2fec
RicheJoe <919091092@qq.com>
2020-08-20 19:05:22 +0800

    初始化

---
+++ app.js
@@ -0,0 +1,42 @@
+App({
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    //场景信息和用户信息
+    // console.log(options);
+    
+    // wx.getUserInfo({
+    //   success: function(res){
+    //     console.log(res);  
+    //   }
+    // })
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
+  },
+
+  GlobalData:{
+    name:'vn',
+    age : 18
+  }
+})
